c30e0b6e3ff00a97c5c15cae96b8198aa45abdc6
dollarkiller <adapawang@gmail.com>
2021-06-22 22:53:48 +0800

    :construction: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          {/*<Route exact path="/" component={HomePage}/>*/}
+          {/*<Route path="/signIn" component={SignInPage}/>*/}
+          <Route render={()=> <h1>404 Not Found</h1>} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
